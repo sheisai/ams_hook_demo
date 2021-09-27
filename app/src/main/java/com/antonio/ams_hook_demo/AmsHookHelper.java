0f84428ee3ec1825d1ecc87a8b07d9285b924bf7
陈永安 <chenya@2345.com>
2021-09-27 09:12:29 +0800

    init:适配一下10.0系统以上

--- app/src/main/java/com/antonio/ams_hook_demo/AmsHookHelper.java
+++ app/src/main/java/com/antonio/ams_hook_demo/AmsHookHelper.java
@@ -1,6 +1,10 @@
 package com.antonio.ams_hook_demo;
 
+import android.os.Build;
+import android.util.Log;
+
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /**
@@ -10,36 +14,39 @@ import java.lang.reflect.Proxy;
  * @version 1.0
  */
 public class AmsHookHelper {
+    private final static String TAG = "AmsHookHelper";
 
     /**
      * hook掉ActivityManager
      */
     public static void hookActivityManager() {
         try {
-            Class<?> activityManagerNativeClass = Class.forName("android.app.ActivityManagerNative");
-
+            Class<?> activityManagerNativeClass = Class.forName("android.app.ActivityTaskManager");
             // 获取 gDefault 这个字段, 想办法替换它
-            Field gDefaultField = activityManagerNativeClass.getDeclaredField("gDefault");
+            Field gDefaultField = activityManagerNativeClass.getDeclaredField("IActivityTaskManagerSingleton");
             gDefaultField.setAccessible(true);
 
-            Object gDefault = gDefaultField.get(null);
-
-            // 5.0 以上的gDefault是一个 android.util.Singleton对象; 我们取出这个单例里面的字段
-            Class<?> singleton = Class.forName("android.util.Singleton");
-            Field mInstanceField = singleton.getDeclaredField("mInstance");
+            Object IActivityManagerSingleton = gDefaultField.get(null);
+            //2.获取mInstance
+            Class<?> singletonClass = Class.forName("android.util.Singleton");
+            Field mInstanceField = singletonClass.getDeclaredField("mInstance");
             mInstanceField.setAccessible(true);
 
-            // ActivityManagerNative 的gDefault对象里面原始的 IActivityManager对象
-            Object rawIActivityManager = mInstanceField.get(gDefault);
+            if (Build.VERSION.SDK_INT >= 29) {
+                //Q上需要动态执行create方法
+                Method getMethod = singletonClass.getMethod("get");
+                getMethod.setAccessible(true);
+                getMethod.invoke(IActivityManagerSingleton);
 
-            // 创建一个这个对象的代理对象, 然后替换这个字段, 让我们的代理对象帮忙干活
-            Class<?> iActivityManagerInterface = Class.forName("android.app.IActivityManager");
-            Object proxy = Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
-                    new Class<?>[]{iActivityManagerInterface}, new AmsHookHandler(rawIActivityManager));
-            mInstanceField.set(gDefault, proxy);
-
+            }
+            Object mInstance = mInstanceField.get(IActivityManagerSingleton);
+            //3.动态代理设置自己的mInstance
+            Object proxyInstance = Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
+                    mInstance.getClass().getInterfaces(),
+                    new AmsHookHandler(mInstance));
+            mInstanceField.set(IActivityManagerSingleton, proxyInstance);
         } catch (Exception e) {
-            throw new RuntimeException("Hook Failed", e);
+            Log.e(TAG, "Hook Failed", e);
         }
 
     }
